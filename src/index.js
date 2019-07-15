ac360cca650a379972d3efa1168e9e6a8be860ed
teddimagg <theodor@codelab.is>
2019-07-15 16:12:27 +0000

    fixes to help dialog

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
 //Packages
-var _               = require('lodash');
-var rename          = require('rename-keys');
-var tabletojson     = require('tabletojson');
-var Promise         = require('promise');
-var chalk           = require('chalk');
+var _ = require('lodash');
+var rename = require('rename-keys');
+var tabletojson = require('tabletojson');
+var Promise = require('promise');
+var chalk = require('chalk');
 var request = require('request');
 var cheerio = require('cheerio');
 
 // Url to fetch vinbudin
 var url = 'http://www.vinbudin.is/heim/vorur/tabid-2311.aspx';
 var openingHours = 'http://www.vinbudin.is/heim/opnunartimar.aspx';
-var cats = [{isl: 'Rauðvín',                      en: 'red'},
-            {isl: 'Hvítvín',                      en: 'white'},
-            {isl: 'Bjór',                         en: 'beer'},
-            {isl: 'Sterkt',                       en: 'strong'},
-            {isl: 'Annað',                        en: 'other'},
-            {isl: 'Eftirréttavín o.fl.',          en: 'desert'},
-            {isl: 'Síder og gosblöndur',          en: 'cider'},
-            {isl: 'Umbúðir og aðrar söluvörur',   en: 'umb'}]
+var cats = [{ isl: 'Rauðvín', en: 'red' },
+{ isl: 'Hvítvín', en: 'white' },
+{ isl: 'Bjór', en: 'beer' },
+{ isl: 'Sterkt', en: 'strong' },
+{ isl: 'Annað', en: 'other' },
+{ isl: 'Eftirréttavín o.fl.', en: 'desert' },
+{ isl: 'Síder og gosblöndur', en: 'cider' },
+{ isl: 'Umbúðir og aðrar söluvörur', en: 'umb' }]
 
 
 //Command constructor
@@ -35,18 +35,18 @@ var ope;
 
 init();
 //Initiates the CLI
-function init(){
+function init() {
     readArgs();
-    if(construct.help){
+    if (construct.help) {
         printHelp();
         return;
     }
 
-    if(construct.open){
+    if (construct.open) {
         Promise.all([fetchOpeningHours()]).then((data) => {
             (data) ? processData() : console.log('Error contacting the liquor store.');
         });
-    } else if(construct.command){
+    } else if (construct.command) {
         Promise.all([fetchProducts()]).then((data) => {
             (data) ? processData() : console.log('Error contacting the liquor store.');
         });
@@ -55,59 +55,59 @@ function init(){
 }
 
 //Reads in cli arguments
-function readArgs(){
+function readArgs() {
     var args = process.argv.slice(2);
-    for(i in args){
-        if(args[i] == 'get' || args[i] == 'find'){
+    for (i in args) {
+        if (args[i] == 'get' || args[i] == 'find') {
             construct.command = args[i];
-        } else if(args[i] == 'open') {
+        } else if (args[i] == 'open') {
             construct.command = args[i];
             construct.open = true;
-        } else if(args[i] == '-h' || args[i] == '--help'){
+        } else if (args[i] == '-h' || args[i] == '--help') {
             construct.help = true;
-        } else if(args[i][0] == '-'){
+        } else if (args[i][0] == '-') {
             construct.sort = args[i].slice(1);
-        } else if(construct.command) {
-            construct.string ? construct.string += ' ' + args[i]: construct.string += args[i];
+        } else if (construct.command) {
+            construct.string ? construct.string += ' ' + args[i] : construct.string += args[i];
         }
     }
-    if(!construct.command || !args.length){ console.log('type: alkinn --help'); return; }
+    if (!construct.command || !args.length) { console.log('type: alkinn --help'); return; }
 }
 
-function processData(){
+function processData() {
     var data = [];
-    if(construct.command === 'get'){
+    if (construct.command === 'get') {
         data = get();
-    } else if(construct.command === 'find'){
+    } else if (construct.command === 'find') {
         data = find(construct.string);
-    } else if(construct.command === 'open'){
+    } else if (construct.command === 'open') {
         printOpeningHours();
         return;
     } else {
         return;
     }
 
-    if(construct.sort){
+    if (construct.sort) {
         data = sortBy(data);
     }
     print(data);
 
 }
 
-function find(string){
+function find(string) {
     var contains = [];
-    for(i in res){
-        if(res[i]['name'].toLowerCase().includes(string.toLowerCase())){
+    for (i in res) {
+        if (res[i]['name'].toLowerCase().includes(string.toLowerCase())) {
             contains.push(res[i]);
         }
     }
     return contains;
 }
 
-function sortBy(obj){
-    if(construct.sort == 'drunk'){
-        return _.sortBy(obj, function(o){
-            if(o){ return (o['volume'] * o['perc'] / 100) / o['price']; }
+function sortBy(obj) {
+    if (construct.sort == 'drunk') {
+        return _.sortBy(obj, function (o) {
+            if (o) { return (o['volume'] * o['perc'] / 100) / o['price']; }
         })
     } else {
         return _.sortBy(obj, construct.sort);
@@ -120,31 +120,32 @@ function sortBy(obj){
     Name        VOL         %     ISK       Country         Cat
 
 */
-function print(obj){
-    console.log(`                                                                 ┌───────────────┐
-    ┌────────────────────────────────────────┬────────────┬──────┤    ALKINN     ├──────┬────────────────────────┬────────────────────────┐
-    │                   Name                 │   Vol      |   %  └───────┬───────┘  ISK │         Country        │        Category        │
+function print(obj) {
+    console.log(`                                                               ┌────────────────┐
+    ┌────────────────────────────────────────┬────────────┬────┤     ALKINN     ├───────┬────────────────────────┬────────────────────────┐
+    │                   Name                 │   Vol      |  % └─────────┬──────┘  ISK  │         Country        │        Category        │
     ├────────────────────────────────────────┼────────────┼──────────────┼──────────────┼────────────────────────┼────────────────────────┤`);
-    for(i in obj){true
-        if(obj[i].category != 'Annað' &&  obj[i].category != 'Umbúðir og aðrar söluvörur'){
+    for (i in obj) {
+        true
+        if (obj[i].category != 'Annað' && obj[i].category != 'Umbúðir og aðrar söluvörur') {
             var str = '';
             str += '    ';
-            for(j in obj[i]){
-                if(validInfo(j)){
+            for (j in obj[i]) {
+                if (validInfo(j)) {
                     var cellSize;
-                    switch (j){
-                        case 'name':     cellSize = 40; break;
-                        case 'volume':   cellSize = 12; obj[i][j] += ' ml'; break;
-                        case 'perc':     cellSize = 14; obj[i][j] += ' %'; break;
-                        case 'price':    cellSize = 14; obj[i][j] += ' kr.'; break;
-                        case 'country':  cellSize = 24; break;
+                    switch (j) {
+                        case 'name': cellSize = 40; break;
+                        case 'volume': cellSize = 12; obj[i][j] += ' ml'; break;
+                        case 'perc': cellSize = 14; obj[i][j] += ' %'; break;
+                        case 'price': cellSize = 14; obj[i][j] += ' kr.'; break;
+                        case 'country': cellSize = 24; break;
                         case 'category': cellSize = 24; break;
                     }
-                    if(obj[i][j].length > 30){
+                    if (obj[i][j].length > 30) {
                         obj[i][j] = obj[i][j].substr(0, 30) + '...';
                     }
-                    var offset = _.repeat(' ', ((cellSize - obj[i][j].length)/2));
-                    if(obj[i][j].length % 2 == 0){
+                    var offset = _.repeat(' ', ((cellSize - obj[i][j].length) / 2));
+                    if (obj[i][j].length % 2 == 0) {
                         str += '|' + offset + obj[i][j] + offset;
                     } else {
                         str += '|' + offset + obj[i][j] + offset + ' ';
@@ -160,17 +161,17 @@ function print(obj){
     console.log('');
 }
 
-function validInfo(j){
+function validInfo(j) {
     return (j === 'name') || (j === 'country') || (j === 'volume') || (j === 'perc') || (j === 'price') || (j === 'category');
 }
 
-function printHelp(){
+function printHelp() {
     let usage = 'usage: alkinn <command> <string | type> [-sortby] [-h | --help]'
     usage += '\n'
-    usage += `\n${chalk.bold('command:')}      get <type>, find <string>, open (opening hours)`
-    usage += `\n${chalk.bold('string:')}       Search parameter using find`
-    usage += `\n${chalk.bold('types:')}        beer, red, white, strong, cider, desert`
-    usage += `\n${chalk.bold('-sortby:')}      name, volume, perc, price, country, category or drunk`
+    usage += `\n${chalk.green('command:')}       get <type>, find <string>, open (opening hours)`
+    usage += `\n${chalk.green('string:')}        Search parameter using find`
+    usage += `\n${chalk.green('types:')}         beer, red, white, strong, cider, desert`
+    usage += `\n${chalk.green('sortby:')}        name, volume, perc, price, country, category or drunk(%*vol/price)`
     usage += '\n\n'
     usage += `\n${chalk.bold('-h | --help:')}  Show these instructions`
 
@@ -178,65 +179,65 @@ function printHelp(){
 }
 
 /*Returns beers*/
-function get(){
-    if(construct.string){
-        return _.filter(res, {category: _.find(cats, {'en': construct.string}).isl});
+function get() {
+    if (construct.string) {
+        return _.filter(res, { category: _.find(cats, { 'en': construct.string }).isl });
     } else {
         return res;
     }
 }
 
-function fetchProducts(){
+function fetchProducts() {
     return new Promise((resolve, reject) => {
         tabletojson.convertUrl(url)
-        .then(function(tablesAsJson) {
-            for(i in tablesAsJson){
-                for(j in tablesAsJson[i]){
-                    //Hvert object
-                    var obj = tablesAsJson[i][j];
-                    var currCountry;
-                    var currCategory;
-                    if(Object.keys(obj).length === 1){
-                        if(isCategory(obj['0'])){
-                            currCategory = obj['0'];
-                        } else {
-                            currCountry = obj['0'];
-                        }
-                        delete tablesAsJson[i][j];
-                    } else {
-                        if(currCategory == 'Umbúðir og aðrar söluvörur' && currCategory == 'Annað'){
+            .then(function (tablesAsJson) {
+                for (i in tablesAsJson) {
+                    for (j in tablesAsJson[i]) {
+                        //Hvert object
+                        var obj = tablesAsJson[i][j];
+                        var currCountry;
+                        var currCategory;
+                        if (Object.keys(obj).length === 1) {
+                            if (isCategory(obj['0'])) {
+                                currCategory = obj['0'];
+                            } else {
+                                currCountry = obj['0'];
+                            }
                             delete tablesAsJson[i][j];
                         } else {
-                            tablesAsJson[i][j] = rename(obj, changeKeys);
-                            tablesAsJson[i][j].country = currCountry;
-                            tablesAsJson[i][j].category = currCategory;
+                            if (currCategory == 'Umbúðir og aðrar söluvörur' && currCategory == 'Annað') {
+                                delete tablesAsJson[i][j];
+                            } else {
+                                tablesAsJson[i][j] = rename(obj, changeKeys);
+                                tablesAsJson[i][j].country = currCountry;
+                                tablesAsJson[i][j].category = currCategory;
 
-                            tablesAsJson[i][j].price = Number(tablesAsJson[i][j].price.slice(0, -4).replace('.', ''));
-                            tablesAsJson[i][j].perc = Number(tablesAsJson[i][j].perc.slice(0, -1));
-                            tablesAsJson[i][j].volume = Number(tablesAsJson[i][j].volume.slice(0, -2));
+                                tablesAsJson[i][j].price = Number(tablesAsJson[i][j].price.slice(0, -4).replace('.', ''));
+                                tablesAsJson[i][j].perc = Number(tablesAsJson[i][j].perc.slice(0, -1));
+                                tablesAsJson[i][j].volume = Number(tablesAsJson[i][j].volume.slice(0, -2));
+                            }
                         }
                     }
                 }
-            }
 
-            if(tablesAsJson){
-                res = tablesAsJson[0];
-                resolve(true);
-            } else {
-                reject(false);
-            }
-        });
+                if (tablesAsJson) {
+                    res = tablesAsJson[0];
+                    resolve(true);
+                } else {
+                    reject(false);
+                }
+            });
     });
 }
 
 
-function fetchOpeningHours(){
+function fetchOpeningHours() {
     return new Promise((resolve, reject) => {
         var openingHoursArr = [];
         request(openingHours, function (error, response, html) {
             if (!error && response.statusCode == 200) {
                 var $ = cheerio.load(html);
-                $('.info').each(function(i, element){
+                $('.info').each(function (i, element) {
                     //console.log($(element).text());
                     var title = $(element).find('.title').text();
                     var address = $(element).find('.address').text();
@@ -252,7 +253,7 @@ function fetchOpeningHours(){
                     openingHoursArr.push(store);
                 });
 
-                if(openingHoursArr){
+                if (openingHoursArr) {
                     ope = openingHoursArr;
                     resolve(true);
                 } else {
@@ -264,8 +265,8 @@ function fetchOpeningHours(){
 }
 
 
-var changeKeys = function(str){
-    switch(str) {
+var changeKeys = function (str) {
+    switch (str) {
         case '0':
             return 'foodcategories';
         case '1':
@@ -283,7 +284,7 @@ var changeKeys = function(str){
     }
 };
 
-var isCategory = function(str){
+var isCategory = function (str) {
     return _.includes(_.map(cats, 'isl'), str);
 }
 
@@ -302,26 +303,26 @@ var store = {
     Title   Opens   Closes      Address
 */
 
-function printOpeningHours(){
+function printOpeningHours() {
     iceland();
     console.log(`                                                   ┌───────────────┐
     ┌──────────────────────────┬───────────────────┤    ALKINN     ├──────────┬──────────┬──────────┐
     │          Title           │   Address         └───────────────┘          │   Open   │  Close   │
     ├──────────────────────────┼──────────────────────────────────────────────┼──────────┼──────────┤`);
-    for(i in ope){
+    for (i in ope) {
         var obj = ope[i];
         var str = '';
         str += '    ';
-        for(var j in obj){
+        for (var j in obj) {
             var cellSize;
             switch (j) {
-                case 'title':     cellSize = 26; break;
-                case 'address':   cellSize = 46; break;
-                case 'openHour':  cellSize = 10; break;
+                case 'title': cellSize = 26; break;
+                case 'address': cellSize = 46; break;
+                case 'openHour': cellSize = 10; break;
                 case 'closeHour': cellSize = 10; break;
             }
-            var offset = _.repeat(' ', ((cellSize - obj[j].length)/2));
-            if(obj[j].length % 2 == 0){
+            var offset = _.repeat(' ', ((cellSize - obj[j].length) / 2));
+            if (obj[j].length % 2 == 0) {
                 str += '|' + offset + obj[j] + offset;
             } else {
                 str += '|' + offset + obj[j] + offset + ' ';
@@ -336,18 +337,18 @@ function printOpeningHours(){
 }
 
 
-function iceland(){
+function iceland() {
     var openings = {};
-    for(i in ope){
+    for (i in ope) {
         var h = new Date().getHours();
         var title = ope[i]['title'].toLowerCase();
-        title = title.replace(/[á]/g,"a");
-        title = title.replace(/[æ]/g,"ae");
-        title = title.replace(/[í]/g,"i");
-        title = title.replace(/[ú]/g,"u");
-        title = title.replace(/[óö]/g,"o");
-        title = title.replace(/[þ]/g,"t");
-        title = title.replace(/[ð]/g,"d");
+        title = title.replace(/[á]/g, "a");
+        title = title.replace(/[æ]/g, "ae");
+        title = title.replace(/[í]/g, "i");
+        title = title.replace(/[ú]/g, "u");
+        title = title.replace(/[óö]/g, "o");
+        title = title.replace(/[þ]/g, "t");
+        title = title.replace(/[ð]/g, "d");
         openings['' + title.split(' ')[0]] = (h >= ope[i].openHour && h < ope[i].closeHour) ? chalk.green('@') : chalk.red('@');
     }
     // console.log(openings);
